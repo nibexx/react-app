b1ecdc807df2c1e6eda2559163298855b25d31c4
nibexx <ebin.ptf@gmail.com>
2025-03-08 15:36:56 +0530

    this is test for react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a sample react application to test github action template.
+          This is a sample react application created by ebin.
         </p>
         <p>sample test</p>
         <a
